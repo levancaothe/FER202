7dc676b6bbab83e86b172fbe3e0760a26048a83e
levancaothe <thel2230@gmail.com>
2026-08-11 12:05:57 +0700

    Update new requirements

--- ex-slot2/src/App.js
+++ ex-slot2/src/App.js
@@ -1,50 +1,99 @@
+import { useState } from "react";
 import { products } from "./models/Product";
+import { Button, Card, Col, Row } from "react-bootstrap";
 
 function App() {
-  const maxPrice = products.reduce((max, product) =>
+  const [productList, setProductList] = useState(products);
+
+  const maxPrice = productList.reduce((max, product) =>
     product.outPrice > max.outPrice ? product : max,
   );
 
-  const minPrice = products.reduce((min, product) =>
+  const minPrice = productList.reduce((min, product) =>
     product.outPrice < min.outPrice ? product : min,
   );
 
-  const productsWithProfit = products
+  const productsWithProfit = productList
     .map((product) => ({
       ...product,
       profit: product.outPrice - product.inputPrice,
     }))
     .sort((a, b) => b.profit - a.profit);
 
+  const handleBuy = (id) => {
+    setProductList((prevProducts) =>
+      prevProducts.map((product) =>
+        product.id === id && product.stock > 0
+          ? {
+              ...product,
+              stock: product.stock - 1,
+            }
+          : product,
+      ),
+    );
+  };
+
   return (
-    <div>
+    <div className="ms-5">
       <h2>Danh sách sản phẩm</h2>
 
-      <table border="1" cellPadding="10">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Tên sản phẩm</th>
-            <th>Giá nhập</th>
-            <th>Giá bán</th>
-            <th>Tồn kho</th>
-            <th>Trạng thái</th>
-          </tr>
-        </thead>
+      <Row className="mt-3 mb-3">
+        {productList.map((product) => (
+          <Col md={3} key={product.id}>
+            <Card>
+              <Card.Img
+                className="card-img-top"
+                src={product.img}
+                alt={product.name}
+                style={{
+                  width: "90%",
+                  height: 200,
+                }}
+              />
 
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td>{product.id}</td>
-              <td>{product.name}</td>
-              <td>{product.inputPrice}</td>
-              <td>{product.outPrice}</td>
-              <td>{product.stock}</td>
-              <td>{product.stock > 0 ? "Còn hàng" : "Hết hàng"}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              <Card.Text className="ms-3">
+                <strong>ID:</strong> {product.id}
+              </Card.Text>
+
+              <Card.Text className="ms-3">
+                <strong>Name:</strong> {product.name}
+              </Card.Text>
+
+              <Card.Text className="ms-3">
+                <strong>Input Price:</strong> {product.inputPrice}
+              </Card.Text>
+
+              <Card.Text className="ms-3">
+                <strong>Out Price:</strong> {product.outPrice}
+              </Card.Text>
+
+              <Card.Text className="ms-3">
+                <strong>Stock:</strong> {product.stock}
+              </Card.Text>
+
+              <Card.Text className="ms-3">
+                <strong>Status:</strong>{" "}
+                {product.stock > 0 ? "Còn hàng" : "Hết hàng"}
+              </Card.Text>
+
+              <div className="text-center">
+                <Button
+                  variant="primary"
+                  className="mb-1"
+                  style={{
+                    backgroundColor: product.stock > 0 ? "blue" : "grey",
+                    width: "50%",
+                  }}
+                  onClick={() => handleBuy(product.id)}
+                  disabled={product.stock === 0}
+                >
+                  {product.stock > 0 ? "Mua ngay" : "Hết hàng"}
+                </Button>
+              </div>
+            </Card>
+          </Col>
+        ))}
+      </Row>
 
       <h2>Giá bán</h2>
 
@@ -57,6 +106,7 @@ function App() {
       </p>
 
       <h2>Sản phẩm theo lợi nhuận giảm dần</h2>
+
       <table border="1" cellPadding="10">
         <thead>
           <tr>
--- ex-slot2/src/models/Product.js
+++ ex-slot2/src/models/Product.js
@@ -1,6 +1,34 @@
 export const products = [
-  { id: 1, name: "Laptop ASUS", inputPrice: 15000, outPrice: 18500, stock: 5 },
-  { id: 2, name: "Chuột", inputPrice: 300, outPrice: 450, stock: 0 },
-  { id: 3, name: "Bàn phím", inputPrice: 800, outPrice: 1200, stock: 10 },
-  { id: 4, name: "Màn hình Dell", inputPrice: 3500, outPrice: 4200, stock: 2 },
+  {
+    id: 1,
+    img: "/images/0P.jpg",
+    name: "Laptop ASUS",
+    inputPrice: 15000,
+    outPrice: 18500,
+    stock: 5,
+  },
+  {
+    id: 2,
+    img: "/images/1P.jpg",
+    name: "Chuột",
+    inputPrice: 300,
+    outPrice: 450,
+    stock: 0,
+  },
+  {
+    id: 3,
+    img: "/images/2P.jpg",
+    name: "Bàn phím",
+    inputPrice: 800,
+    outPrice: 1200,
+    stock: 10,
+  },
+  {
+    id: 4,
+    img: "/images/3P.jpg",
+    name: "Màn hình Dell",
+    inputPrice: 3500,
+    outPrice: 4200,
+    stock: 2,
+  },
 ];
